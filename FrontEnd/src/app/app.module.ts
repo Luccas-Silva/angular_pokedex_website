17270e96e3232709043c50e8e7008ae4803f73fa
Luccas Silva <lluccas.anjoss@gmail.com>
2023-01-22 01:28:55 -0300

    Creating Page Module - Creating shared module and shared components

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PagesModule } from './pages/pages.module';
+import { DetailsComponent } from '.page/details/details.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
