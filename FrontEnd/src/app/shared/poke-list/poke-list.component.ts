dce3bb8296ebe0f0f584542fd676aa9de1c0e1d3
Luccas Silva <lluccas.anjoss@gmail.com>
2023-01-23 08:38:38 -0300

    Emitindo eventos

--- FrontEnd/src/app/shared/poke-list/poke-list.component.ts
+++ FrontEnd/src/app/shared/poke-list/poke-list.component.ts
@@ -7,7 +7,7 @@ import { PokeApiService } from 'src/app/service/poke-api.service'
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent  implements OnInit {
+export class PokeListComponent implements OnInit {
   
   public getAllPokemons: any;
 
