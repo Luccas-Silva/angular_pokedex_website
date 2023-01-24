fb82602bc2a5e8ff2ddbcb045063fa6afbdefdb8
Luccas Silva <lluccas.anjoss@gmail.com>
2023-01-24 12:43:43 -0300

    Estilo da página de details - Estilo de botões

--- FrontEnd/src/app/pages/details/details.component.ts
+++ FrontEnd/src/app/pages/details/details.component.ts
@@ -13,6 +13,8 @@ export class DetailsComponent implements OnInit{
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+
   constructor(
     private activateRoute: ActivatedRoute,
     private PokeApiService: PokeApiService
@@ -20,17 +22,17 @@ export class DetailsComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
   }
 
-  get pokemon(){
+  get getPokemon(){
     const id = this.activateRoute.snapshot.params['id'];
     const pokemon = this.PokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.PokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
 
     return forkJoin([pokemon, name]).subscribe(
       res => {
-        console.log(res);
+        this.pokemon = res;
       }
     );
 
